512bd0fa6fdf78ff89b7a7f8e4a27a2e4eb077e8
Gabriel Oshiro <gabriel.oshiro@cbyk.com.br>
2020-09-10 10:21:36 -0300

    Utilizando axios

--- main.js
+++ main.js
@@ -1,23 +1,4 @@
-var minhaPromise = function () {
-	return new Promise(function (resolve, reject) {
-		var xhr = new XMLHttpRequest();
-
-		xhr.open('GET', 'https://api.github.com/users/gabrieloshiro1997');
-		xhr.send(null);
-
-		xhr.onreadystatechange = function () {
-			if (xhr.readyState === 4) {
-				if (xhr.status === 200) {
-					resolve(JSON.parse(xhr.responseText));
-				} else {
-					reject('Erro na requisição');
-				}
-			}
-		}
-	})
-}
-
-var resultado = minhaPromise()
+axios.get('https://api.github.com/users/gabrieloshiro1997')
 	.then(function(response) {
 		console.log(response);
 	})
